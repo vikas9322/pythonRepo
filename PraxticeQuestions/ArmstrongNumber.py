264ebb843d61a8630774111839b3e58a463d0553
vikas <panditvikas18@gmail.com>
2024-03-02 22:41:53 +0530

    solving armstrong number

--- PraxticeQuestions/ArmstrongNumber.py
+++ PraxticeQuestions/ArmstrongNumber.py
@@ -1,54 +1,72 @@
-# Function to calculate x raised to 
-# the power y
+# # Function to calculate x raised to 
+# # the power y
 
 
-def power(x,y):
-    if y == 0:
-        return 1
-    if y % 2 ==0:
-        return power(x,y // 2) * power(x,y // 2)
-    return x * power(x,y // 2) * power(x,y // 2)
+# def power(x,y):
+#     if y == 0:
+#         return 1
+#     if y % 2 ==0:
+#         return power(x,y // 2) * power(x,y // 2)
+#     return x * power(x,y // 2) * power(x,y // 2)
 
-def order(x):
-    n = 0
-    while( x != 0):
-        n = n +1
-        x = x // 10
+# def order(x):
+#     n = 0
+#     while( x != 0):
+#         n = n +1
+#         x = x // 10
 
-    return n
+#     return n
 
-def isArmstrong(x):
-    n = order(x)
-    temp = x
-    sum1 = 0
+# def isArmstrong(x):
+#     n = order(x)
+#     temp = x
+#     sum1 = 0
 
-    while(temp != 0):
-        r = temp % 10
-        sum1 = sum1 + power(r,n)
-        temp = temp // 10
+#     while(temp != 0):
+#         r = temp % 10
+#         sum1 = sum1 + power(r,n)
+#         temp = temp // 10
 
-    return (sum1 == x)
-
-
-x = 1534
-print(isArmstrong(x))
+#     return (sum1 == x)
 
 
-print("##################################################")
+# x = 1534
+# print(isArmstrong(x))
 
-#Armstrong number without using the power function
 
-n = int(input("Enter the number of your choice "))
-s = n
-b = len(str(n)) 
-sum1 = 0
+# print("##################################################")
+
+# #Armstrong number without using the power function
+
+# n = int(input("Enter the number of your choice "))
+# s = n
+# b = len(str(n)) 
+# sum1 = 0
  
+# while n != 0:
+#     r = n % 10
+#     sum1 = sum1 + (r**b)
+#     n = n//10
+# if s == sum1:
+#     print("The given number",s, " is armstrong number")
+# else:
+#     print("The given number ", s , "si not armstong number")
+
+
+# Armstrong number without using the power function
+
+n = 153
+s = n
+b = len(str(n))
+sum1 = 0
+
 while n != 0:
     r = n % 10
     sum1 = sum1 + (r**b)
-    n = n//10
+    n = n // 10
+
 if s == sum1:
-    print("The given number",s, " is armstrong number")
+    print("the give number is armstrong number", s)
 else:
-    print("The given number ", s , "si not armstong number")
+    print("the given number is not armstrong number",s)
 
